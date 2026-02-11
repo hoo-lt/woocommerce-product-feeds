bac3a33e64f548f3c1481f18cf417e3a3d4a43c3
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-02-11 18:13:35 +0200

    slug added

--- src/Application/Dtos/Products/Product/Product.php
+++ src/Application/Dtos/Products/Product/Product.php
@@ -7,6 +7,7 @@ readonly class Product
 	public function __construct(
 		public int $id,
 		public string $name,
+		public string $slug,
 		public float $price,
 		public int $stock,
 		public string $gtin,
